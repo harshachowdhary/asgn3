fe20b873ea538b7cb3f8470c37d87e05bca81929
harshachowdhary <144712425+harshachowdhary@users.noreply.github.com>
2023-10-27 23:00:57 -0400

    h1
    h1

---
+++ asgn 3 final 1/eLearning/src/elearning/models/CourseModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elearning.models;
+
+/**
+ *
+ * @author harsh
+ */
+public class CourseModel {
+    
+    private String courseName;
+    private SubjectModel subjectObj;
+    
+}
---
+++ asgn 3 final 1/eLearning/src/elearning/models/SemisterModel.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elearning.models;
+
+import java.util.List;
+
+/**
+ *
+ * @author harsh
+ */
+public class SemisterModel {
+
+    private String semisterName;
+    private List<SubjectModel> subjects;
+    
+    /**
+     * @return the subjects
+     */
+    public List<SubjectModel> getSubjects() {
+        return subjects;
+    }
+
+    /**
+     * @param subjects the subjects to set
+     */
+    public void setSubjects(List<SubjectModel> subjects) {
+        this.subjects = subjects;
+    }
+
+    /**
+     * @return the semisterName
+     */
+    public String getSemisterName() {
+        return semisterName;
+    }
+
+    /**
+     * @param semisterName the semisterName to set
+     */
+    public void setSemisterName(String semisterName) {
+        this.semisterName = semisterName;
+    }
+    
+    
+    
+}
